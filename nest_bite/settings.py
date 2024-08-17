647e9c4b2ac9dbe7a796e30cdaff8bcfe3c35ebe
Valerie Solinas <valerius92@live.it>
2024-08-17 09:45:51 +0000

    update allowed_hosts

--- nest_bite/settings.py
+++ nest_bite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^v7-lym8335243&^e^!mr==w^$9u9a^pstp(wa6o$zc-=*dj39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-valerios05-nestbite-sttxgmzo342.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-valerios05-nestbite-sttxgmzo342.ws.codeinstitute-ide.net','nest_bite.herokuapp.com','.herokuapp.com']
 
 
 # Application definition
