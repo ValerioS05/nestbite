b27a6eb65b8c347a606bfdea584a2fdef3fd27ac
Valerie Solinas <valerius92@live.it>
2024-09-09 10:55:34 +0000

    update models Restaurant, Table add str and meta

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant,Table
 # Register your models here.
 
 
--- restaurants/models.py
+++ restaurants/models.py
@@ -8,19 +8,30 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=150)
     description = models.TextField()
     phone_number = models.CharField(max_length=25)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)                       # automatic time stamp when created
     capacity = models.PositiveIntegerField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     owner = models.ForeignKey(
-        User, on_delete=models.PROTECT
+        User, on_delete=models.PROTECT                                      # Restaurant protected from delete (connected to User)
     )
 
+    def __str__(self):
+        return f"{self.name}"
+    
+    class Meta:
+        ordering = ['name']                                                   # ordering by name
 
 class Table(models.Model):
-    restaurant = models.ForeignKey(
-        Restaurant, on_delete=models.CASCADE, related_name='tables'
+    restaurant = models.ForeignKey(                                           # related to Restaurant model
+        Restaurant, on_delete=models.CASCADE, related_name='tables'           # deleted if restaurant is deleted
         )
     table_number = models.CharField(max_length=3)
     capacity = models.IntegerField()
+    
+    def __str__(self):
+        return f"Table {self.table_number} (Max:{self.capacity}) at {self.restaurant} "
+
+    class Meta:
+        ordering = ['table_number', 'restaurant']
 
