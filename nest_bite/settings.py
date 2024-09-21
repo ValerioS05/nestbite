4fe546998176f82027ad2b490ab700a8854816cc
Valerie Solinas <valerius92@live.it>
2024-09-21 11:37:51 +0000

    add summernote for admin panel ,update related admin,settings,urls

--- nest_bite/settings.py
+++ nest_bite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-valerios05-nestbite-x0yb3k3gjuz.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'restaurants'
 ]
 
--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Restaurant,Table
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Restaurant)
+class RestaurantAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'capacity', 'opening_time', 'closing_time')
+    search_fields = ['name']
+    list_filter = ('name',)
+    summernote_fields = ('description',)
 # Register your models here.
 
 
-admin.site.register(Restaurant)
+
 admin.site.register(Table)
