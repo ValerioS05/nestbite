28d6674839bee44130ead5ba25859bc1af54e39c
Valerie Solinas <valerius92@live.it>
2024-10-21 15:58:42 +0000

    create test_booking(auto test), forms.py(Python linter)

--- booking/forms.py
+++ booking/forms.py
@@ -1,6 +1,10 @@
-from django import forms # Import the forms module from Django
-from .models import Booking, Review # Import the Booking model from the current app
-from restaurants.models import Table, Restaurant  # Import the Table and Restaurant models from the restaurants app
+from django import forms
+# Import the forms module from Django
+from .models import Booking, Review
+# Import the Booking model from the current app
+from restaurants.models import Table, Restaurant
+# Import the Table and Restaurant models from the restaurants app
+
 
 # Define form based on the Booking model
 class BookingForm(forms.ModelForm):
@@ -12,18 +16,25 @@ class BookingForm(forms.ModelForm):
 
     class Meta:
         model = Booking  # specify which model
-        fields = ['tables', 'customer_name', 'booking_date', 'start_time', 'end_time', 'message'] # Fields included in the form
+        fields = ['tables', 'customer_name', 'booking_date',
+                  'start_time', 'end_time', 'message']
 
     def __init__(self, *args, **kwargs):
-        restaurant_id = kwargs.pop('restaurant_id', None) # Get the restaurant id from keywords
+        restaurant_id = kwargs.pop('restaurant_id', None)
+        # Get the restaurant id from keywords
         super().__init__(*args, **kwargs)
 
         # Filter tables based on the selected restaurant
-        if restaurant_id: # Check if an id was provided
-            self.fields['tables'].queryset = Table.objects.filter(restaurant_id=restaurant_id) # Filter tables of specified id
+        if restaurant_id:  # Check if an id was provided
+            self.fields['tables'].queryset = Table.objects.filter(
+                restaurant_id=restaurant_id
+            )  # Filter tables of specified id
 
             self.fields['tables'].choices = [
-                (table.id, f"Table {table.table_number} (Max: {table.capacity}) - £{table.price:.2f}")  # Display the table and price
+                (table.id, f"""
+                    Table {table.table_number} (Max: {table.capacity})
+                    - £{table.price:.2f}""")
+                # Display the table and price
                 for table in self.fields['tables'].queryset
             ]
 
@@ -31,13 +42,29 @@ class BookingForm(forms.ModelForm):
             restaurant = Restaurant.objects.get(id=restaurant_id)
             opening_time = restaurant.opening_time
             closing_time = restaurant.closing_time
-            
+
             # Set widgets for date
-            self.fields['booking_date'].widget = forms.DateInput(attrs={'type': 'date'})  # Date picker
+            self.fields['booking_date'].widget = forms.DateInput(
+                attrs={'type': 'date'}
+            )  # Date picker
 
             # Set initial value and attributes for start_time
-            self.fields['start_time'].widget = forms.TimeInput(attrs={'type': 'time', 'min': opening_time, 'max': (closing_time.hour - 1)})
-            self.fields['end_time'].widget = forms.TimeInput(attrs={'type': 'time', 'min': f"{(opening_time.hour + 1)}:00", 'max': closing_time})
+            self.fields['start_time'].widget = forms.TimeInput(
+                attrs={
+                    'type': 'time',
+                    'min': opening_time,
+                    'max': (closing_time.hour - 1)
+                }
+            )
+
+            self.fields['end_time'].widget = forms.TimeInput(
+                attrs={
+                    'type': 'time',
+                    'min': f"{(opening_time.hour + 1)}:00",
+                    'max': closing_time
+                }
+            )
+
 
 # Define form based on the Review model
 class ReviewForm(forms.ModelForm):
@@ -45,12 +72,15 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ['rating', 'message']
         widgets = {
-            'rating': forms.RadioSelect(choices=[
-                (1, '1 Star'),
-                (2, '2 Stars'),
-                (3, '3 Stars'),
-                (4, '4 Stars'),
-                (5, '5 Stars')
-            ]),
-            'message': forms.Textarea(attrs={'placeholder': 'Leave a message (optional)', 'rows': 4}),
+            'rating': forms.RadioSelect(
+                choices=[
+                    (1, '1 Star'), (2, '2 Stars'), (3, '3 Stars'),
+                    (4, '4 Stars'), (5, '5 Stars')
+                ]
+            ),
+            'message': forms.Textarea(
+                attrs={
+                    'placeholder': 'Leave a message (optional)', 'rows': 4
+                }
+            ),
         }
---
+++ booking/tests_booking.py
@@ -0,0 +1,62 @@
+from django.urls import reverse
+from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta
+from .models import Booking, Table
+from django.contrib.auth.models import User
+from restaurants.models import Restaurant
+
+class BookingTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass'
+        )
+
+        self.restaurant = Restaurant.objects.create(
+            name='Test Restaurant',
+            address='123',
+            description='great place to eat.',
+            phone_number='1234567890',
+            capacity=20,
+            opening_time='10:00',
+            closing_time='22:00',
+            owner=self.user,
+        )
+
+        self.table = Table.objects.create(
+            restaurant=self.restaurant,
+            table_number='1',
+            capacity=4,
+            price=10.00
+        )
+
+    def test_create_booking(self):
+        self.client.login(username='testuser', password='testpass')
+
+        start_time = (timezone.now() + timedelta(hours=2, minutes=1)).time()
+        end_time = (timezone.now() + timedelta(hours=3, minutes=1)).time()
+
+        booking_data = {
+            'customer_name': 'Test User',
+            'customer_email': 'test@example.com',
+            'booking_date': timezone.now().date(),
+            'start_time': start_time,
+            'end_time': end_time,
+            'tables': [self.table.id],
+            'user': self.user.id,
+        }
+
+        response = self.client.post(
+            reverse('create_booking', args=[self.restaurant.id]),
+            booking_data
+        )
+
+        print("Response status code:", response.status_code)
+        print("Response content:", response.content)
+
+        self.assertEqual(Booking.objects.count(), 1, "No booking was created")
+        booking = Booking.objects.first()
+        self.assertEqual(booking.customer_name, 'Test User')
+        self.assertEqual(booking.user, self.user)
+        self.assertIn(self.table, booking.tables.all())
