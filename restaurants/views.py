1cd2f518c05ef07a7892aec6ce1cae3d012b1b3d
Valerie Solinas <valerius92@live.it>
2024-10-24 16:08:58 +0000

    update restaurants view and template to include pagination

--- restaurants/views.py
+++ restaurants/views.py
@@ -12,7 +12,8 @@ def index(request):
 class RestaurantList(generic.ListView):
     template_name = "restaurants.html"
     context_object_name = "restaurants"
-
+    paginate_by = 3
+    
     def get_queryset(self):
         return restaurant_filter(self.request)
 
